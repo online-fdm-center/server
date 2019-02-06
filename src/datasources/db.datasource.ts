49d9b8843f156cdc03386b2a6d809d766654c680
dilap54 <dilap54@mail.ru>
2019-02-06 08:40:16 +0000

    Данные для подключения к MySQL через переменные окружения

--- src/datasources/db.datasource.ts
+++ src/datasources/db.datasource.ts
@@ -1,13 +1,17 @@
 import { inject } from '@loopback/core';
 import { juggler } from '@loopback/repository';
-import * as config from './db.datasource.json';
 
 export class DbDataSource extends juggler.DataSource {
   static dataSourceName = 'db';
 
   constructor(
     @inject('datasources.config.db', { optional: true })
-    dsConfig: object = config,
+    dsConfig: object = {
+      name: 'db',
+      connector: 'mysql',
+      url: process.env.MYSQL_URL,//'mysql://root:123321Aa@database/online.fdm.center',
+    }
+    ,
   ) {
     super(dsConfig);
   }
